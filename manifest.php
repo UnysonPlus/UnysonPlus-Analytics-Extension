1a5a195afc945190978c259ef5e6904dab6a23a6
jonmlas <jonmlas@gmail.com>
2025-08-21 18:38:32 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,18 +1,31 @@
 <?php if ( ! defined( 'FW' ) ) {
-	die( 'Forbidden' );
+    die( 'Forbidden' );
 }
 
 $manifest = array();
 
 $manifest['name']        = __( 'Analytics', 'fw' );
-$manifest['description'] = __( 'Enables the possibility to add the Google Analytics tracking code that will let you get all the analytics about visitors, page views and more.', 'fw' );
-$manifest['version'] = '1.0.1';
-$manifest['display'] = true;
-$manifest['standalone'] = true;
+$manifest['slug']        = 'unysonplus-analytics'; // unique identifier
+$manifest['description'] = __( 
+    'Adds Google Analytics tracking support, enabling visitor and page view tracking within your WordPress site.', 
+    'fw' 
+);
 
-$manifest['github_update'] = 'ThemeFuse/Unyson-Analytics-Extension';
+$manifest['version']     = '1.0.2';
+$manifest['display']     = true;
+$manifest['standalone']  = true;
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Analytics-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/introduction.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+// Repo Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Analytics-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Analytics-Extension';
+$manifest['github_branch'] = 'master'; // optional, can use 'stable'
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Extra Meta
+$manifest['license']        = 'GPL-2.0-or-later';
+$manifest['text_domain']    = 'fw'; 
+$manifest['requires_php']   = '7.4'; 
+$manifest['requires_wp']    = '5.8';
